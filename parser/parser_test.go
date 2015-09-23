c707d77c80b823937ce4326d2cbfbd4d7b1725c7
Radomir Sohlich <sohlich@gmail.com>
2015-09-23 04:53:27 +0200

    Tests fixed, no use of java

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,29 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseDocument(t *testing.T) {
-	pluginMap["java"] = &Plugin{
-		Path:       "../plugin/JPlag.jar",
-		Language:   "java",
-		Extensions: []string{"c", "cpp"},
-		FileFilter: map[string]bool{"c": true, "cpp": true},
-	} //redefine path to plugin
-
-	input := strings.NewReader(fakeFile)
-	pluginLang := "java"
-	output, err := execJavaPlugin(input, pluginLang)
-	if err != nil {
-		t.Error(err)
-	}
-	decoder := json.NewDecoder(strings.NewReader(output))
-
-	decodedDoc := NGramDoc{}
-	decoder.Decode(&decodedDoc)
-	if !assert.True(t, len(decodedDoc.NGrams) != 0 && len(decodedDoc.Metrics) != 0) {
-		t.Error("TestParseDocument - no output from plugin")
-	}
-}
-
 func TestLoadPlugins(t *testing.T) {
 	files, _ := readFilesFromDir("../plugin/")
 
